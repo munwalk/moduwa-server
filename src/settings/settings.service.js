58553044d60f5a2656c5ef5568fd9b2ca2474c3d
jeongbam <bellajw292@gmail.com>
2026-01-29 00:47:11 +0900

    [refactor]: MOCK 데이터 주석 처리

--- src/settings/settings.service.js
+++ src/settings/settings.service.js
@@ -1,13 +1,13 @@
 import { getOrCreateUserSettings } from "./settings.repository.js";
 
 export const getGridColumns = async ({ userId }) => {
-  // ✅ [추가] 로컬 mock 모드면 DB 건드리지 말고 기본값 반환
-  if (
-    process.env.NODE_ENV === "development" &&
-    process.env.MOCK_DB === "true"
-  ) {
-    return 7;
-  }
+  // MOCK
+  //   if (
+  //     process.env.NODE_ENV === "development" &&
+  //     process.env.MOCK_DB === "true"
+  //   ) {
+  //     return 7;
+  //   }
   const settings = await getOrCreateUserSettings({ userId });
 
   return settings.gridColumns ?? 7;
